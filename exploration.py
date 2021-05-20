a096ffba46ed6aac2a50570893ff82ced6b4346d
Niklas Weber <nalderan@gmail.com>
2021-05-21 01:48:27 +0200

    float64 -> float16 and english noun list filtering

--- exploration.py
+++ exploration.py
@@ -12,46 +12,54 @@ NDARR = np.ndarray
 
 
 def main() -> None:
-    glove_path: str = './embeddings/glove/6b/glove.6B.300d.txt'
+    glove_path: str = "./embeddings/glove/6b/glove.6B.300d.txt"
+    # glove_path: str = "./embeddings/glove/840b/glove.840B.300d.txt"
     glove: PDF = pd.read_csv(
         filepath_or_buffer=glove_path,
         delim_whitespace=True,
-        engine='c',
+        engine="c",
         header=None,
         index_col=0,
         # nrows=100000,
         quoting=csv.QUOTE_NONE,
-        dtype={0: object}.update({i: np.float16 for i in range(1, 301)})
     )
 
-    wordlist_ids = [
-        'ceadmilefailte',
-        'default',
-        'duet',
-        'thegamegal',
-        'thegamegal'
-    ]
+    # can't do this while reading, ignored by csv parser, see here:
+    # https://stackoverflow.com/questions/24761122/pandas-read-csv-ignoring-column-dtypes-when-i-pass-skip-footer-arg
+    dtypes: dict = {i: np.float16 for i in range(1, len(glove.columns) + 1)}
+    glove = glove.astype(dtypes)
+
+    # nouns in the english language
+    en_noun_path: str = './english_noun_lists/great_noun_list/nounlist.txt'
+    with open(en_noun_path, 'r') as f:
+        # lowercase to match glove
+        # read().splitlines() instead of readlines() to get rid of
+        # trailing newlines
+        en_nouns: list = [word.lower() for word in f.read().splitlines()]
+
+    nouns_with_glove = set(en_nouns).intersection(set(glove.index))
+    glove = glove.loc[nouns_with_glove]
+
+    wordlist_ids = ["ceadmilefailte", "default", "duet", "thegamegal", "thegamegal"]
 
     codewords: set = set()
     for wordlist_id in wordlist_ids:
-        wordlist_path_template: str = \
-            './wordlists/sagelga/wordlist/en-EN/{}/wordlist.txt'
+        wordlist_path_template: str = "./wordlists/sagelga/wordlist/en-EN/{}/wordlist.txt"
         wl_path: str = wordlist_path_template.format(wordlist_id)
-        with open(wl_path, 'r') as f:
+        with open(wl_path, "r") as f:
             # lowercase to match glove
             # read().splitlines() instead of readlines() to get rid of
             # trailing newlines
-            words: set = set([word.lower() for word in f.read(
-            ).splitlines()])
+            words: set = set([word.lower() for word in f.read().splitlines()])
 
         codewords = codewords.union(words)
 
     codewords_l: list = list(codewords)
     rand_state = np.random.RandomState(0)
 
-    for i in range(3):
+    for i in range(5):
         # make some riddles
-        riddle: NDARR = rand_state.choice(codewords_l, 3, replace=False)
+        riddle: NDARR = rand_state.choice(codewords_l, 2, replace=False)
 
         # can we find some vectors for this?
         try:
@@ -59,23 +67,25 @@ def main() -> None:
         except KeyError as e:
             print("not all words of riddle contained in glove vecs")
             print(f"riddle: {riddle}")
-            print(e)
             continue
 
         # average of those
         vec_avg = vecs.mean()
 
+        # don't want a riddle word to be a possible hint
+        glove_no_riddle = glove[~glove.index.isin(riddle)]
+
         # now to back-translate that into a word, find closest glove vec to this
         # euclidean distances
-        dists: NDARR = np.linalg.norm(glove.values.astype(np.float16) -
-                                      vec_avg.values.astype(np.float16),
-                                      axis=1)
+        dists: NDARR = np.linalg.norm(
+            glove_no_riddle.values.astype(np.float16) - vec_avg.values.astype(np.float16), axis=1
+        )
         hint_ix: int = dists.argmin()
-        hint: str = glove.iloc[hint_ix].name
+        hint: str = glove_no_riddle.iloc[hint_ix].name
 
         print(f"riddle: {riddle}")
         print(f"hint: {hint}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
