2d1699aa71e549de293ec248417bde7e9aace4a0
Niklas Weber <nalderan@gmail.com>
2021-05-29 17:41:05 +0200

    lots of refactoring, extracting chunks of code into function

--- exploration.py
+++ exploration.py
@@ -104,64 +104,21 @@ def generate_hints(riddle: list, glove: PDF, k: int = 5) -> list:
 
 
 def main() -> None:
-    # glove_path: str = "./embeddings/glove/6b/glove.6B.300d.txt"
-    # glove_path: str = "./embeddings/glove/840b/glove.840B.300d.txt"
     glove_path: str = "./embeddings/glove/840b/glove.840B.300d.txt.python_filtered"
-    glove_raw: PDF = pd.read_csv(
-        filepath_or_buffer=glove_path,
-        delim_whitespace=True,
-        engine="c",
-        header=None,
-        index_col=0,
-        quoting=csv.QUOTE_NONE,
-    )
+    glove_raw: PDF = load_glove(glove_path)
 
-    # can't do this while reading, ignored by csv parser, see here:
-    # https://stackoverflow.com/questions/24761122/pandas-read-csv-ignoring-column-dtypes-when-i-pass-skip-footer-arg
-    dtypes: dict = {i: np.float16 for i in range(1, len(glove_raw.columns) + 1)}
-    glove_raw = glove_raw.astype(dtypes)
-
-    # words (mostly nouns though) in the english language
-    # great noun list
-    # http://www.desiquintans.com/nounlist
-    gr_n_l_path: str = "./english_noun_lists/great_noun_list/nounlist.txt"
-    with open(gr_n_l_path, "r") as f:
-        # lowercase to match glove
-        # read().splitlines() instead of readlines() to get rid of
-        # trailing newlines
-        gr_n_l_nouns: set = set([word.lower() for word in f.read().splitlines()])
-
-    # webster's unabridged
-    # https://www.gutenberg.org/ebooks/author/139
-    wbstr_pth: str = "./english_noun_lists/webster_unabridged/extracted_nouns.txt"
-    with open(wbstr_pth, "r") as f:
-        wbstr_nouns: set = set([word.lower() for word in f.read().splitlines()])
-
-    # dwyl english word list
-    # https://github.com/dwyl/english-words/
-    dywl_pth: str = "./english_noun_lists/dwyl_english_words/words_alpha.txt"
-    with open(dywl_pth, "r") as f:
-        dwyl_words: set = set([word.lower() for word in f.read().splitlines()])
-
-    wordlist_ids = ["ceadmilefailte", "default", "duet", "thegamegal", "thegamegal"]
-    codewords: set = set()
-    for wordlist_id in wordlist_ids:
-        wordlist_path_template: str = "./wordlists/sagelga/wordlist/en-EN/{}/wordlist.txt"
-        wl_path: str = wordlist_path_template.format(wordlist_id)
-        with open(wl_path, "r") as f:
-            # lowercase to match glove
-            # read().splitlines() instead of readlines() to get rid of
-            # trailing newlines
-            words: set = set([word.lower() for word in f.read().splitlines()])
-        codewords = codewords.union(words)
+    words: dict = load_known_words_all_sources()
 
     # write this out for other uses
-    accepted_hints: set = wbstr_nouns.union(gr_n_l_nouns).union(codewords)
+    accepted_hints: set = words["wbstr_nouns"].union(words["gr_n_l_nouns"]).union(
+        words["codenames"]
+    )
     # artifact from webster parsing upstream
     accepted_hints.remove("")
     with open("./accepted_hints.txt", "w") as f:
         f.writelines("\n".join(accepted_hints))
 
+    # indexing into glove df fails if there are unknown words, thus intersect with df index
     glv_wrds_accept_set: set = set(glove_raw.index).intersection(accepted_hints)
     # TODO words in Glove are Case Sensitive, but my 'accepted hints' are not, cutting out too many words
     glove_filtered = glove_raw.loc[glv_wrds_accept_set]
@@ -180,6 +137,82 @@ def main() -> None:
         data=zip(ALL_TEST_CASES, hints), columns=["riddle", "hints"]
     )
     info(overview)
+
+
+def load_known_words_all_sources():
+    words = dict()
+    # words (mostly nouns though) in the english language
+    # great noun list
+    # http://www.desiquintans.com/nounlist
+    gr_n_l_path: str = "./english_noun_lists/great_noun_list/nounlist.txt"
+    gr_n_l_nouns = load_great_noun_list(gr_n_l_path)
+    words["gr_n_l_nouns"] = gr_n_l_nouns
+    # webster's unabridged
+    # https://www.gutenberg.org/ebooks/author/139
+    wbstr_pth: str = "./english_noun_lists/webster_unabridged/extracted_nouns.txt"
+    wbstr_nouns = load_webster_nouns(wbstr_pth)
+    words["wbstr_nouns"] = wbstr_nouns
+    # dwyl english word list
+    # https://github.com/dwyl/english-words/
+    dywl_pth: str = "./english_noun_lists/dwyl_english_words/words_alpha.txt"
+    dwyl_words: set = load_dwyl_words(dywl_pth)
+    words["dwyl_words"] = dwyl_words
+    # official words of various codenames editions
+    codenames: set = load_codenames()
+    words["codenames"] = codenames
+    return words
+
+
+def load_codenames():
+    wordlist_ids = ["ceadmilefailte", "default", "duet", "thegamegal", "thegamegal"]
+    codewords: set = set()
+    for wordlist_id in wordlist_ids:
+        wordlist_path_template: str = "./wordlists/sagelga/wordlist/en-EN/{}/wordlist.txt"
+        wl_path: str = wordlist_path_template.format(wordlist_id)
+        with open(wl_path, "r") as f:
+            # lowercase to match glove
+            # read().splitlines() instead of readlines() to get rid of
+            # trailing newlines
+            words: set = set([word.lower() for word in f.read().splitlines()])
+        codewords = codewords.union(words)
+    return codewords
+
+
+def load_dwyl_words(dywl_pth):
+    with open(dywl_pth, "r") as f:
+        dwyl_words: set = set([word.lower() for word in f.read().splitlines()])
+    return dwyl_words
+
+
+def load_webster_nouns(wbstr_pth):
+    with open(wbstr_pth, "r") as f:
+        wbstr_nouns: set = set([word.lower() for word in f.read().splitlines()])
+    return wbstr_nouns
+
+
+def load_great_noun_list(gr_n_l_path):
+    with open(gr_n_l_path, "r") as f:
+        # lowercase to match glove
+        # read().splitlines() instead of readlines() to get rid of
+        # trailing newlines
+        gr_n_l_nouns: set = set([word.lower() for word in f.read().splitlines()])
+    return gr_n_l_nouns
+
+
+def load_glove(glove_path):
+    glove_raw: PDF = pd.read_csv(
+        filepath_or_buffer=glove_path,
+        delim_whitespace=True,
+        engine="c",
+        header=None,
+        index_col=0,
+        quoting=csv.QUOTE_NONE,
+    )
+    # can't do this while reading, ignored by csv parser, see here:
+    # https://stackoverflow.com/questions/24761122/pandas-read-csv-ignoring-column-dtypes-when-i-pass-skip-footer-arg
+    dtypes: dict = {i: np.float16 for i in range(1, len(glove_raw.columns) + 1)}
+    glove_raw = glove_raw.astype(dtypes)
+    return glove_raw
 
 
 if __name__ == "__main__":
